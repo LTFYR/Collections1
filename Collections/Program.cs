5a8a4427bd404bb6a9af6e6d2b5975546614be6a
LTFYR <lutfiyariea@code.edu.az>
2022-03-15 23:57:50 +0400

    Add project files.

---
+++ Collections/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collections
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList prog = new SortedList();
+            prog.Add("C#", 2000);
+            prog.Add("JAVA", 1995);
+            prog.Add("Python", 1991);
+            prog.Add("C++", 1985);
+
+            foreach (DictionaryEntry item in prog)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+        }
+    }
+}
